b486f62b1331c9605d2f42f4e6e209c8c7c37c95
Daniel Berrocal <jdanielb1992@gmail.com>
2014-03-14 21:56:49 -0600

    form Login y conexión
    no estoy muy seguro de la conexión

--- Mamirmi/Mamirmi/Program.cs
+++ Mamirmi/Mamirmi/Program.cs
@@ -16,7 +16,7 @@ namespace Mamirmi
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
