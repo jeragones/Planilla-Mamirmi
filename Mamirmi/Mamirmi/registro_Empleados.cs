4840d078fa9eae875577ecee0850acbe3fdd028d
Christiam Andres Suazo López <suazocrisl@gmail.com>
2014-03-17 20:20:04 -0600

    validaciones empleados

--- Mamirmi/Mamirmi/registro_Empleados.cs
+++ Mamirmi/Mamirmi/registro_Empleados.cs
@@ -37,21 +37,29 @@ namespace Mamirmi
 
         private void btn_AgregarEmpleado_Click(object sender, EventArgs e)
         {
-            //instancia.insertar_Empleado("11","1","bncuenta","bpcuenta", 
-             //   "Gerente", "Universidad","Curso general","Ninguna","123456",
-              //  "4 años","daddadsa","Administrativo",null,null,"Activo");
-            instancia.insertar_Empleado(cmbPersonaid.Text,txt_cuentaBN.Text,txtCuentaBp.Text,
-                txtProfesion.Text,cmbEstudios.Text,txtManipulacionA.Text,txtEnfermedades.Text,
-                txtCarne.Text,txtExperiencia.Text,txtFIngreso.Text,cmbDepartamento.Text,"","","Activo");
-            txt_cuentaBN.Text ="";
-            txtCuentaBp.Text ="";
-            txtProfesion.Text ="";
-            txtManipulacionA.Text ="";
-            txtEnfermedades.Text ="";
-            txtCarne.Text ="";
-            txtExperiencia.Text ="";
-            txtFIngreso.Text=""; 
-
+            if (txt_cuentaBN.Text.Equals("") || txtCuentaBp.Text.Equals("") || txtProfesion.Text.Equals("") || txtManipulacionA.Text.Equals("")
+               || txtEnfermedades.Text.Equals("") || txtCarne.Text.Equals("") || txtExperiencia.Text.Equals("") || txtFIngreso.Text.Equals(""))
+            {
+                MessageBox.Show("Debe llenar todos los datos", "Alerta",
+                   MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+            else
+            {
+                //instancia.insertar_Empleado("11","1","bncuenta","bpcuenta", 
+                //   "Gerente", "Universidad","Curso general","Ninguna","123456",
+                //  "4 años","daddadsa","Administrativo",null,null,"Activo");
+                instancia.insertar_Empleado(cmbPersonaid.Text, txt_cuentaBN.Text, txtCuentaBp.Text,
+                    txtProfesion.Text, cmbEstudios.Text, txtManipulacionA.Text, txtEnfermedades.Text,
+                    txtCarne.Text, txtExperiencia.Text, txtFIngreso.Text, cmbDepartamento.Text, "", "", "Activo");
+                txt_cuentaBN.Text = "";
+                txtCuentaBp.Text = "";
+                txtProfesion.Text = "";
+                txtManipulacionA.Text = "";
+                txtEnfermedades.Text = "";
+                txtCarne.Text = "";
+                txtExperiencia.Text = "";
+                txtFIngreso.Text = ""; 
+            }
         }
 
         private void empleadosToolStripMenuItem_Click(object sender, EventArgs e)
